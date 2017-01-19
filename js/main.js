f5ea3c3a29a248c199f55d2fb621ecf959de7772
KAMA Hiroki <s207ae08k@gmail.com>
2017-01-19 07:59:28 +0000

    find modification

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function () {
     var speed = 500;
     var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
+    var position = target.offset().top + 90;
     $("html, body").animate({
       scrollTop: position
     }, speed, "swing");
