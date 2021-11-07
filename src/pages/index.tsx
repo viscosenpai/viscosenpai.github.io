81a47b8943e9c77eb2c2cd851126cf368945cc96
viscosenpai <kama.hiroki@gmail.com>
2021-11-07 23:48:52 +0900

    [add] md記事一覧表示とページ遷移確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,36 @@
 import { Box, chakra } from '@chakra-ui/react';
 import React from 'react';
+import { getAllPosts } from '../lib/api';
+import Post from '../types/post';
 
-export default function Home() {
+type Props = {
+  allPosts: Post[];
+};
+
+export default function Home({ allPosts }: Props) {
   return (
-    <Box>
-      <chakra.h1 color="tomato">Hello World!</chakra.h1>
-    </Box>
+    <>
+      <Box>
+        <chakra.h1 color="tomato">Hello World!</chakra.h1>
+      </Box>
+      <ul>
+        {allPosts.map((post) => {
+          return (
+            <li key={post.slug}>
+              <p>{post.title}</p>
+              <p>{post.date}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts(['title', 'date', 'slug']);
+
+  return {
+    props: { allPosts },
+  };
+};
---
+++ src/types/post.ts
@@ -0,0 +1,10 @@
+type PostType = {
+  slug: string;
+  title: string;
+  date: string;
+  draft: boolean;
+  path: string;
+  content: string;
+};
+
+export default PostType;
